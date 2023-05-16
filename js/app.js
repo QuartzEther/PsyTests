737d8e70a3801165d56e4d290dd943daefb3df40
QuartzEther <quartz.ether@gmail.com>
2023-05-16 19:19:25 +0300

    json new & create descr from json

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ window.onload = startPreloadChain;
 const imgArr = [];
 let data = null;
 
+//------------------------------------------------
+const container = document.querySelector('.container');
+
+//--------------Load Data & sources---------------
 function startPreloadChain(){
     console.log('startPreloadChain');
     loadData();
@@ -26,22 +30,24 @@ function onDataPreloaded(){
     console.log('onDataPreloaded');
 
     findPict(data);
-    console.log(data['img_src']);
+    preloadPictures(data['srcImg'], imgArr, init);
 
-    preloadPictures(data['img_src'], imgArr, init);
 }
 
 function findPict(array){
     for (let key in array){
-        if (typeof array[key] === 'object') {
-            findPict(array[key])
+        if (key.includes('img')) {
+            if (typeof array[key] === 'object'){
+                for (let temp of array[key]) imgArr.push(temp);
+            } else {
+                imgArr.push(array[key]);
+            }
         }
-        else if (key === 'img') {
-            imgArr.push(array[key]);
+        else if (typeof array[key] === 'object') {
+            findPict(array[key])
         }
     }
 }
-
 
 function preloadPictures(path ,sources, callback) {
     const images = [];
@@ -58,8 +64,107 @@ function preloadPictures(path ,sources, callback) {
     }
 }
 
-
+//----------------------INIT------------------------
 function init()
 {
-    console.log(123)
+    console.log("init")
+
+    addElement(data.description, 'description');
+}
+
+function addElement(sectionData, sectionType){
+
+    let sectionStruct = [];
+    let sectionText = [];
+    let sectionImg = [];
+    let sectionBtn = null;
+
+    //structure of section & init
+
+    if (sectionData.text) sectionText = sectionData.text.split(/[|{}]/);
+
+    for (let i in sectionText){
+        if (sectionText[i].includes('img')){
+            sectionStruct.push('img');
+            sectionImg.push(sectionData[sectionText[i]]);
+            sectionText[i] = "";
+        }else {
+            if (sectionText[i])
+                sectionStruct.push('text');
+        }
+    }
+    sectionText = sectionText.filter((n) => {return n != ""});
+
+    if (sectionData.btn){
+        sectionBtn = sectionData.btn;
+        sectionStruct.push('btn');
+    }
+
+    //-----------Create DOM section element-------------
+
+    let section = document.createElement('div')
+    container.append(section);
+
+    //tittle
+    let temp = document.createElement('h1');
+    temp.innerHTML = data.name;
+    temp.classList.add('tittle');
+    section.append(temp);
+
+    //inner
+    let inner = document.createElement('div');
+    inner.classList.add('inner');
+    section.append(inner);
+
+    for (let el of sectionStruct){
+        if (el === 'text'){
+            temp = document.createElement('p');
+
+            temp.classList.add("inner__text", "text");
+            temp.innerHTML = sectionText.shift();
+
+            inner.append(temp);
+
+        }else if (el === 'img'){
+
+            let tempSrc = data["srcImg"]+sectionImg.shift();
+
+            temp = document.createElement('div');
+
+            temp.classList.add("inner__img", "img");
+            temp.style.backgroundImage = `url("${tempSrc}")`;
+            temp.style.height = Math.floor(getImgParam(tempSrc, 'height')*0.75) + 'px'
+
+
+            inner.append(temp);
+
+        }else if (el === 'btn'){
+            temp = document.createElement('button');
+
+            temp.classList.add("btn")
+            temp.innerHTML = sectionBtn;
+
+            section.append(temp);
+        }
+    }
+
+    if (sectionType == 'description'){
+        section.classList.add('description-section');
+        inner.classList.add('description-section__inner')
+
+        temp = section.querySelector('.tittle');
+        if (temp) temp.classList.add('description-section__tittle')
+
+        temp = section.querySelector('.btn');
+        if (temp) temp.classList.add('description-section__btn')
+    }
+}
+
+
+//----------Get imgWidth-----------
+function getImgParam(src, param){
+    let tempImg = document.createElement('img');
+    tempImg.src = src;
+
+    return tempImg[param];
 }
